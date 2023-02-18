8a5cb11fbd159a86b20b5a88350dabc43f24629b
Jarod Smith <lbriones.dev@gmail.com>
2023-02-18 20:08:49 -0300

    Funcionando Menu Principal/Crear Usuarios/Mostrar Usuarios

--- PRUEBA_DIAGNOSTICO/m4ABPRO4/src/m4ABPRO4/M4APBRO4.java
+++ PRUEBA_DIAGNOSTICO/m4ABPRO4/src/m4ABPRO4/M4APBRO4.java
@@ -1,25 +1,37 @@
 package m4ABPRO4;
 
-import java.util.Arrays; //para utilizar el array
+//import java.util.Arrays; //para utilizar el array
 import java.util.Scanner;
 
 
 public class M4APBRO4 {
+	
+	//MATRIZ
 	static String [][] usuarios = new String[100][10];
+	
+	//OBJETO SCANNER
 	static Scanner scanner = new Scanner(System.in);
+	
+	//EXPRESIONES REGULARES
 	static String regExNumerica = "^[0-9]+$"; //SÓLO ACEPTA CARACTERES NUMÉRICOS
-	static int filaLibre; 
+	static String RegExRut = "^\\d{1,8}-[0-9K]$"; //CAMBIAR A 6 EL 1 EN PRODUCCCION DEBUG****
+	static String regExTelefono = "^\\+?[0-9]{1,3}[-\\s]?\\(?\\d{3}\\)?[-\\s]?\\d{3}[-\\s]?\\d{4}$";
+	static String regExFecha = "^(0?[1-9]|[12][0-9]|3[01])\\/(0?[1-9]|1[0-2])\\/((?:19|20)[0-9]{2}|18[0-9]{2})|\\b29\\/02\\/((?:19|20)(?:04|08|[2468][048]|[13579][26])|(?:2000))$";
+	
+	//VARIABLES GLOBALES
+	static int filaLibre;
+	static boolean primerInicio = true;
 
-	static String nombre;  //0
-	static String fechaNacimiento; //1
-	static String rut; //2
-	static String direccion; //3
-	static String telefono; //4
-	static String cantEmpleados; //5
-	static String experiencia; //6
-	static String departamento; //7
-	static String funcion; //8
-	static String nombresuperior; //9
+//	static String nombre;  //0
+//	static String fechaNacimiento; //1
+//	static String rut; //2
+//	static String direccion; //3
+//	static String telefono; //4
+//	static String cantEmpleados; //5
+//	static String experiencia; //6
+//	static String departamento; //7
+//	static String funcion; //8
+//	static String nombresuperior; //9
 
 
 	public static void main(String[] args) {
@@ -28,23 +40,98 @@ public class M4APBRO4 {
 		usuarios[0] = new String[]{"Juan", "01/01/1990", "12345678-9", "Calle 1 #123", "+56912345678", "10", "5 años", "Ventas", "Vendedor", "Pedro"};
 		usuarios[1] = new String[]{"María", "02/02/1995", "98765432-1", "Calle 2 #456", "+56987654321", "5", "2 años", "Marketing", "Analista", "Sofía"};
 		usuarios[2] = new String[]{"Pedro", "03/03/1985", "1-9", "Calle 3 #789", "+56911111111", "20", "10 años", "Finanzas", "Contador", "Carlos"};
-		filaLibre = posicionRegistroVacio(usuarios);
-		menuPerfilUsuario();
-		//registrarUsuario(usuarios);
-		mostrarUsuarios(usuarios);
+		
+		menuPrincipal();
+		
 	}
 	
 	//REGISTRA USUARIOS EN EL ARRAY OBLIGATORIO 1 DE CADA UNO, 3 USUARIOS EN TOTAL
-//	public static void registrarUsuario(String[][]listadoUsuario) {
-//
-//		}
+	public static void registrarUsuario(String[][]listadoUsuario) {
+		if(primerInicio) {
+			System.out.println("[!] ¡SE HA DETECTADO QUE ESTE ES EL PRIMER INICIO!: DEBEMOS INICIAR CON 3 PEFILES DE USUARIOS PARA CONTINUAR");
+			System.out.println("[!] PRIMER PERFIL DE USUARIO A CREAR DEBE SER TIPO CLIENTE\n");
+			scanner.nextLine();
+			filaLibre = posicionRegistroVacio(usuarios);
+			guardarDatosComunes(usuarios);
+			guardarCliente();
+			
+			System.out.println("[!] SEGUNDO PERFIL DE USUARIO A CREAR DEBE SER DE TIPO PROFESIONAL");    		
+			filaLibre = posicionRegistroVacio(usuarios);
+			guardarDatosComunes(usuarios);
+			guardarProfesional();
+			
+			System.out.println("[!] TERCER PERFIL DE USUARIO A CREAR DEBE SER DE TIPO ADMINISTRATIVO");    		
+			filaLibre = posicionRegistroVacio(usuarios);
+			guardarDatosComunes(usuarios);
+			guardarAdministrativo();
+			
+			//CAMBIA LA VARIABLE PARA LA PROXIMA VEZ NO OBLIGUE EL INGRESO DE 3 USUARIOS
+			primerInicio=false;
+			
+			//REGRESA AL MENU PRINCIPAL
+			menuPrincipal();			
+		}else {
+			scanner.nextLine();
+			filaLibre = posicionRegistroVacio(usuarios);
+			guardarDatosComunes(usuarios);
+			menuPerfilUsuario();
+			menuPrincipal();
+		}
+	}
 	
-	//Función selección tipo de usuario
+	//AUTORA: PRISCILA CARRILLO
+	public static void menuPrincipal() {
+		int opcion = 0;
+        
+       	System.out.println("-------------------------------------------------------");
+       	System.out.println("SISTEMA DE USUARIOS\n");
+        System.out.println("[+] Por favor, seleccione una opción:\n");
+        System.out.println("\t1. Registrar usuario");
+        System.out.println("\t2. Mostrar usuarios");
+        System.out.println("\t3. Contar usuarios por categoría");
+        System.out.println("\t4. Modificar usuarios");
+        System.out.println("\t5. Eliminar usuario");
+        System.out.println("\t6. Salir");
+        System.out.println("-------------------------------------------------------");
+        
+        //si se ingresa un número que no está en la lista, debe salir "Error" u "Opción inválida" en la consola//
+        
+        while(opcion != 6) {   //ciclo while hasta que seleccione la opcion 6//
+        	
+        opcion = scanner.nextInt();
+        
+	        switch(opcion) {     //Se usa switch para llamar a la función//
+	            case 1:
+	            	 System.out.println("Registrando usuario...presione Enter para continuar");
+	            	registrarUsuario(usuarios);
+	                break;
+	            case 2:
+	            	System.out.println("Mostrando usuarios...");
+	                mostrarUsuarios(usuarios);
+	                break;
+	            case 3:
+	                //contarUsuariosPorCategoria();
+	                break;
+	            case 4:
+	                //modificarUsuario();
+	                break;
+	            case 5:
+	                //eliminarUsuario();
+	                break;
+	            case 6:
+	                System.out.println("Gracias, hasta pronto.");
+	                break;
+	            default:
+	                System.out.println("[ERROR] Opción inválida. Por favor, seleccione una opción del menú de 1 - 6.");
+	                break;            
+	        }
+        }
+	}
+	
+	//MENÚ SELECCIÓN DE PERFIL
 	public static void menuPerfilUsuario() {
 		String capturador = "";
 		do {
-			//GUARDA RUT/NOMBRE/FECHA NACIMIENTO QUE ES TRANSVERSAL A TODOS LOS USUARIOS
-			guardarDatosComunes(usuarios);
 
 			//MUESTRA MENÚ DE SELECCION EN CONSOLA
 			System.out.println("[+] SELECCIONE EL PERFIL DE USUARIO A CREAR");
@@ -72,7 +159,7 @@ public class M4APBRO4 {
 		}while(!capturador.matches("^[1-3]$"));
 	}
 	
-	//Función que retorna true si existe un rut en el array
+	//BUSCA EL RUT Y RETORNA TRUE O FALSE, RECIBE RUT COMO ARGUMENTO
 	public static boolean existeRegistro(String rut, String[][]listadoUsuario) {
 		for(int i = 0; i < listadoUsuario.length ; i++) {
 	        if(rut.equals(listadoUsuario[i][2])) {
@@ -82,7 +169,7 @@ public class M4APBRO4 {
 	    return false;
 	}
 	
-	//Función que encuentra la primera fila vacía enla columna RUT
+	//RETORNA EL PRIMER REGISTRO VACÍO EN EL ARRAY
 	public static int posicionRegistroVacio(String[][]listadoUsuario) {
 		int posicionVacio = 0;
 		for(int i = 0; i < listadoUsuario.length; i++) {
@@ -93,39 +180,39 @@ public class M4APBRO4 {
 				posicionVacio = -1;
 			}
 		}
-		System.out.println("HE ENCONTRADO LA POSICION " + posicionVacio + " DEL ARRAY DISPONIBLE"); //DEBUG***
+		//System.out.println("HE ENCONTRADO LA POSICION " + posicionVacio + " DEL ARRAY DISPONIBLE"); //DEBUG***
 		return posicionVacio;
 	}
 	
 	//FUNCION PERMITE ALMACENAR LOS DATOS COMUNES A TODOS LOS PERFILES
 	public static void guardarDatosComunes(String[][]usuarios) {
-	String capturador = "";
-	String RegExRut = "^\\d{1,8}-[0-9K]$"; //CAMBIAR A 6 EL 1 EN PRODUCCCION DEBUG****
+		String capturador = "";
 		//RUT: PETICION Y VALIDACION
 			do {
 				System.out.println("[+] Favor ingrese RUT: [OBLIGATORIO]");
-				capturador = scanner.nextLine().trim();
+				capturador = scanner.nextLine();
+				
 				if(capturador.length() == 0) {
-					System.out.println("[ERROR] Debe ingresar RUT para continuar.");
 				}else if(!capturador.matches(RegExRut)) {
 					System.out.println("[ERROR] Formato ingresado no es válido, sólo se permiten dígitos de 6 a 8 un guión y dígito verificador");
 				}else if(existeRegistro(capturador, usuarios)) {
 					//EXISTE USUARIO NO PUEDE CONTINUAR
-					System.out.println("[ERROR]: Usuario ya Existe en la base de datos");
+					System.out.println("[ERROR]: RUT ingresado ya existe en la base de datos");
 				}else {
 					//NO EXISTE USUARIO PUEDE GUARDAR
 					//PROCESO DE GUARDADO DE DATOS EN EL ARRAY
 					usuarios[filaLibre][2] = capturador;
 				}
 			}while(capturador.length() == 0
-					|| existeRegistro(capturador, usuarios) && usuarios[filaLibre][2] != capturador
 					|| !capturador.matches(RegExRut)
+					|| (existeRegistro(capturador, usuarios) && usuarios[filaLibre][2] != capturador)
 					);
 			
 		//NOMBRE: PETICION Y GUARDADO
 				do {
 					System.out.println("[+] Favor Ingrese Nombre : [OBLIGATORIO]");
 					capturador = scanner.nextLine().trim();
+					
 					if(capturador.length()==0) {
 						System.out.println("[ERROR] Nombre Obligatorio, favor reintente.");
 					}else {
@@ -135,7 +222,6 @@ public class M4APBRO4 {
 				}while(capturador.length()==0);
 				
 		//FECHA NACIMIENTO: PETICION Y GUARDADO
-		String regExFecha = "^(0?[1-9]|[12][0-9]|3[01])\\/(0?[1-9]|1[0-2])\\/((?:19|20)[0-9]{2}|18[0-9]{2})|\\b29\\/02\\/((?:19|20)(?:04|08|[2468][048]|[13579][26])|(?:2000))$";
 				do {
 					System.out.println("[+] Favor Ingrese Fecha Nacimiento : [DD/MM/AAAA]");
 					capturador = scanner.nextLine().trim();
@@ -162,9 +248,9 @@ public class M4APBRO4 {
 				usuarios[filaLibre][3] = capturador;
 			}
 		}while(capturador.length() == 0);
-		
+
 	//TELEFONO: PETICION Y GUARDADO
-		String regExTelefono = "^\\+?[0-9]{1,3}[-\\s]?\\(?\\d{3}\\)?[-\\s]?\\d{3}[-\\s]?\\d{4}$";
+
 		do {
 			System.out.println("[+] Favor Ingrese Teléfono : [OBLIGATORIO]");
 			capturador = scanner.nextLine().trim();
@@ -240,17 +326,19 @@ public class M4APBRO4 {
 		usuarios[filaLibre][9] = capturador;
 
 	}
+	
 	//AUTOR: ANDRÉS CONTRERAS	
-	//MUESTRA USUARIOS EN PANTALLA HACIENDO USO DE LA FUNCION imprimirUsuario
 	public static void mostrarUsuarios(String[][] usuarios) {
 		for (String[] usuario : usuarios) { //Creacion de variable auxiliar
 			if (usuario[2] != null) { // Se buscan los usuarios utilizando la variable rut
 				imprimirUsuario(usuario);
 			}
 		}
+		//REGRESA EL MENÚ PRINCIPAL
+		menuPrincipal();
 	}
+	
 	//AUTOR: ANDRÉS CONTRERAS
-	//IMPRIME USUARIO EN PANTALLA
 	public static void imprimirUsuario(String[] usuario) {
 		for (String campo : usuario) {
 			System.out.print(campo + " ");
