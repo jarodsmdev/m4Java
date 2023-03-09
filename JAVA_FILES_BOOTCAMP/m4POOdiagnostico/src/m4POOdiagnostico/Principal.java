523b3a2afd08393c1bd8514674b5fecafc2ce728
Jarod Smith <lbriones.dev@gmail.com>
2023-03-09 12:07:08 -0300

    alpha 0.7.1| FrontLine

--- JAVA_FILES_BOOTCAMP/m4POOdiagnostico/src/m4POOdiagnostico/Principal.java
+++ JAVA_FILES_BOOTCAMP/m4POOdiagnostico/src/m4POOdiagnostico/Principal.java
@@ -10,7 +10,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		Tablero tablero = new Tablero();
 		tablero.crearCarros();
-		tablero.mostrarPlano();
+		//tablero.mostrarPlano();
 		tablero.menu();
 	}
 	
