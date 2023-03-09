163b4f4fc9f076cb53d854144a573065e6e3f62b
Jarod Smith <lbriones.dev@gmail.com>
2023-03-09 10:21:11 -0300

    alpha 0.6|FrontLine

--- JAVA_FILES_BOOTCAMP/m4POOdiagnostico/src/m4POOdiagnostico/Carro.java
+++ JAVA_FILES_BOOTCAMP/m4POOdiagnostico/src/m4POOdiagnostico/Carro.java
@@ -7,42 +7,84 @@ public class Carro {
     private String fechaIngreso;
     private ArrayList<int[][]> coordenadas;
 
-    //CONSTRUCTOR
+    // Constructor por defecto sin parámetros.
+
     public Carro(){}
-    
+
+    /**
+     * Constructor que recibe como parámetros la cantidad de ocupantes y fecha de ingreso del carro.
+     * @param cantOcupantes La cantidad de ocupantes del carro.
+     * @param fechaIngreso La fecha de ingreso del carro.
+     */
     public Carro(int cantOcupantes, String fechaIngreso) {
-    	this.cantOcupantes = cantOcupantes;
-    	this.fechaIngreso = fechaIngreso;
+        this.cantOcupantes = cantOcupantes;
+        this.fechaIngreso = fechaIngreso;
     }
 
+    /**
+     * Método para obtener la cantidad de ocupantes del carro.
+     * @return La cantidad de ocupantes del carro.
+     */
     public int getCantOcupantes(){
         return cantOcupantes;
     }
+
+    /**
+     * Método para establecer la cantidad de ocupantes del carro.
+     * @param cantOcupantes La cantidad de ocupantes del carro.
+     */
     public void setCantOcupantes(int cantOcupantes){
         this.cantOcupantes = cantOcupantes;
     }
+
+    /**
+     * Método para obtener la fecha de ingreso del carro.
+     * @return La fecha de ingreso del carro.
+     */
     public String getFechaIngreso(){
         return fechaIngreso;
     }
+
+    /**
+     * Método para establecer la fecha de ingreso del carro.
+     * @param fechaIngreso La fecha de ingreso del carro.
+     */
     public void setFechaIngreso(String fechaIngreso){
         this.fechaIngreso = fechaIngreso;
     }
+
+    /**
+     * Método para obtener las coordenadas del carro en el tablero.
+     * @return Las coordenadas del carro en el tablero.
+     */
     public ArrayList<int[][]> getCoordenadas(){
         return coordenadas;
     }
+
+    /**
+     * Método para establecer las coordenadas del carro en el tablero.
+     * @param coordenadas Las coordenadas del carro en el tablero.
+     */
     public void setCoordenadas(ArrayList<int[][]> coordenadas) {
         this.coordenadas = coordenadas;
     }
-    //METODO MOSTRAR DATOS
+
+    /**
+     * Método para mostrar los datos del carro, incluyendo la cantidad de ocupantes y la fecha de ingreso.
+     */
     public void mostrarDatos() {
         System.out.println("Ocupantes: " + cantOcupantes);
         System.out.println("Fecha de ingreso: " + fechaIngreso);
-        //System.out.println("Ubicación en el tablero: (" + getCoordenadas() + ", " + getCoordenadas() + ")");
-        
     }
-    
+
+    /**
+     * Método para representar el objeto carro en formato de cadena de texto.
+     * @return El objeto carro representado en formato de cadena de texto.
+     */
     @Override
     public String toString() {
-    	return "Carro [cantOcupantes=" + cantOcupantes + ", fechaIngreso=" + fechaIngreso + "]";
-    }  
+        return "Carro [cantOcupantes=" + cantOcupantes + ", fechaIngreso=" + fechaIngreso + "]";
+    }
+
+
 }
