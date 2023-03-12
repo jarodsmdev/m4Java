63f046e9ff2afd16c4f8f1dc7e6237179eaa890a
Jarod Smith <lbriones.dev@gmail.com>
2023-03-12 01:34:03 -0300

    BETA 1.0.1 FRONTLINE

---
+++ JAVA_FILES_BOOTCAMP/m4POOdiagnostico/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"m4POOdiagnostico","l":"m4POOdiagnostico"}];updateSearchResults();
---
+++ JAVA_FILES_BOOTCAMP/m4POOdiagnostico/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"m4POOdiagnostico","l":"Tablero"}];updateSearchResults();
